8fe3c1fe1906e8b4a2fb33e479a18b46db1c13ee
Julien Tattevin <julien@tattevin.fr>
2024-08-30 21:03:56 +0000

    Change url to remove conflict

--- src/TvSchedule/Controller/TVScheduleAtController.php
+++ src/TvSchedule/Controller/TVScheduleAtController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/api/tv-schedule/at/{requestedTime}')]
+#[Route('/api/tv-schedule-at/{requestedTime}')]
 class TVScheduleAtController extends AbstractController
 {
     public function __construct(
